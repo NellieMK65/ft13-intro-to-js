d0eedcbd35d5bfb4c3097eaba3a2a2f6d6a17816
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2025-03-03 16:47:34 +0300

    into to expressions

---
+++ intro.js
@@ -0,0 +1,20 @@
+// Syntax -> rules associated with a programming language/tool
+
+// Single line comment
+
+/**
+ * Multiline comment
+ *
+ */
+console.log(2 + 2);
+console.log(2 + (3 - 5));
+console.log('Hello World');
+
+/**
+ * Expression
+ * 1. Constant
+ * 2. Assignment
+ * 3. Lookup
+ *
+ * Variables in js
+ */
